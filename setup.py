b6a2e18941ad2a99ab08e6232dd70f342eefcdf1
navya099 <130723074+navya099@users.noreply.github.com>
2026-05-02 10:31:51 +0900

    init.py 추가 및 setup 오타수정

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="MyAlignmnet",
+    name="MyAlignment",
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        "AutoCAD>=0.1.0"
+        "autocad>=0.1.0",
+        "numpy"
     ],
 )
